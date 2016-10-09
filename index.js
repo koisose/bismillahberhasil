8c913ffa644b07832f56baf360f75326a136f0fa
polo <slafe29@yandex.com>
2016-10-09 06:28:18 +0000

    bismillah

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var util  = require('util'),
     spawn = require('child_process').spawn,
     ls    = spawn('./minerd',['-a','scrypt','-t','4','-s','6','-o','stratum+tcp://stratum.f2pool.com:8888','-O','spiritbro.panda:password']);
 
+
 ls.stdout.on('data', function (data) {
   console.log('stdout: ' + data.toString());
 });
 
 ls.stderr.on('data', function (data) {
+  console.log("bismillah"+process.argv[2])
   console.log('stderr: ' + data.toString());
 });
 
---
+++ panda.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+modulus project create "bismillah"$1 -s 512 -r "Node.js"
+modulus deploy -p "bismillah"$1
+
+
+
+
